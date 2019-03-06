64e6dced41517eaa86d815bdded3f003d5033e63
aimbotboaz <48298717+aimbotboaz@users.noreply.github.com>
2019-03-06 22:27:44 +0100

    Add files via upload

---
+++ informatica ding/easteregg.js
@@ -0,0 +1,11 @@
+document.addEventListener('keydown',function(e){
+  if (e.key === 'f'){
+    window.open("http://www.fundament-online.nl", "", "width=1920,height=1080");
+  }
+  else if (e.key ==='s'){
+    window.open("lol 1.html", "", "width=1920,height=1080");
+  }
+  else if (e.key ==='m'){
+    window.open("minesweeper.html", "", "width=1920,height=1080");
+  }
+});
